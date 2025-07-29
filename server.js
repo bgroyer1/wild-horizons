0006a6c1106291d42f8f8f3d04b67b9db622716a
Braedan Royer <royerbraedan@gmail.com>
2025-07-29 11:35:30 -0700

    created repo

---
+++ server.js
@@ -0,0 +1,9 @@
+// go to terminal, type: node server.js
+console.log('Hello node!')
+
+// this will fail, as there is no document object 
+//console.log(document)
+
+// type in node and press enter to start a new node environment
+// this allows us to REPL, read, evaluate, print, loop
+
